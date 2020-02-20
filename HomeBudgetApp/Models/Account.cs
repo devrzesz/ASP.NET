f06233be9feb67f1e900e3f2e88de09109305400
Ariel R <rzeszowsky@gmail.com>
2020-02-20 13:58:04 +0100

    Add Account and Account Type class

---
+++ HomeBudgetApp/Models/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeBudgetApp.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public AccountType Type { get; }
+        public decimal Balance { get; set; }
+        public decimal OpeningBalance { get; set; }
+
+        public Account(decimal openingBalance)
+        {
+            OpeningBalance = openingBalance = Balance;
+        }
+    }
+}
---
+++ HomeBudgetApp/Models/AccountType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeBudgetApp.Models
+{
+    public class AccountType
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
