c72ceadd42db68f135ae7f6137d78464ff4e0e55
Ariel R <rzeszowsky@gmail.com>
2020-02-20 11:57:02 +0100

    Init first commit
    Empty MVC project with single user autenth

---
+++ HomeBudgetApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeBudgetApp.Startup))]
+namespace HomeBudgetApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
