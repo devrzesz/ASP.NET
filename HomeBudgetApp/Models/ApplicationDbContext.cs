040ddee4082da249000e6c1f1f011063055fab87
Ariel R <rzeszowsky@gmail.com>
2020-02-20 14:17:56 +0100

    Enable Migartion and Init Initial Migration, Update Database

--- HomeBudgetApp/Models/ApplicationDbContext.cs
+++ HomeBudgetApp/Models/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace HomeBudgetApp.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AccountType> AccountTypes { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
