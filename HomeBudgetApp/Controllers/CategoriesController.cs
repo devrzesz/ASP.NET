992b7de4edd3c4bcd8179db1ec8528635493e87a
Ariel R <rzeszowsky@gmail.com>
2020-02-25 15:26:15 +0100

    AddSimpleCRUDForCategories

--- HomeBudgetApp/Controllers/CategoriesController.cs
+++ HomeBudgetApp/Controllers/CategoriesController.cs
@@ -9,12 +9,62 @@ namespace HomeBudgetApp.Controllers
 {
     public class CategoriesController : BaseController
     {
-        // GET: Categories
         public ActionResult Index()
         {
             var categories = _context.Categories.ToList();
-
             return View(categories);
+        }
+
+        public ActionResult New()
+        {
+            var category = new Category();
+            return View("Form", category);
+        }
+
+        public ActionResult Save(Category category)
+        {
+            if (!ModelState.IsValid)
+                return View("Form", category);
+
+
+            if (category.CategoryId == 0)
+            {
+                _context.Categories.Add(category);
+
+                TempData["ActionResultEfect"] = $"Category was successfully added";
+            }
+            else
+            {
+                var categoryDB = _context.Categories.Single(c => c.CategoryId == category.CategoryId);
+                categoryDB.Name = category.Name;
+
+                TempData["ActionResultEfect"] = $"Category was successfully changed";
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var category = _context.Categories.SingleOrDefault(c => c.CategoryId == id);
+
+            if (category == null)
+                return HttpNotFound();
+
+            return View("Form", category);
+        }
+
+        public void Delete (int id)
+        {
+            var category = _context.Categories.SingleOrDefault(c => c.CategoryId == id);
+
+            if (category == null)
+                return;
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
         }
     }
 }
